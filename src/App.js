86fd848a8d7dfe9617f3d126b16e19554dacc388
aziz-codes <azizisoffline@gmail.com>
2022-12-03 00:20:18 +0500

    basic image usaage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import myImage from "./images/pic.jpeg";
 const App = () => {
   return (
-    <div className="text-red-500 sm:text-green-500 md:text-sky-500 lg:text-slate-600 xl:text-yellow-500">
-      App
+    <div className="max-w-3xl w-96 bg-red-500 max-h-screen h-screen flex flex-col lg:flex-row items-center">
+      <img
+        src={myImage}
+        alt="profile"
+        className="p-2 max-w-3xl w-60 h-60 rounded-full object-fill max-h-full"
+      />
+      <h4>Aziz</h4>
     </div>
   );
 };
