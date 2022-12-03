1aca8c6958b0a5bf59491298f29a6f433a71233f
aziz-codes <azizisoffline@gmail.com>
2022-12-03 23:13:18 +0500

    mini sidebar for settings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./Navbar";
 import { Routes, Route } from "react-router-dom";
 import { Home, Analytics, Blogs, Profile, Users } from "./pages/index";
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import Settings from "./layouts/Settings";
 const App = () => {
   return (
     <div className="w-full h-full flex flex-col gap-2">
@@ -15,6 +17,10 @@ const App = () => {
           <Route path="/users" element={<Users />} />
         </Routes>
       </div>
+      <div className="fixed right-4 bottom-4 p-1 bg-slate-100 shadow-xl rounded-full">
+        <Cog6ToothIcon className="h-7 w-7 cursor-pointer hover:scale-95 transition-all ease-in-out duration-100" />
+      </div>
+      <Settings />
     </div>
   );
 };
---
+++ src/layouts/Settings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="fixed right-4 bottom-16 h-96 w-56 z-50 max-w-3xl bg-white shadow-lg border ">
+      <div className="absolute h-4 w-4 bg-white rotate-45 -bottom-1 right-2 "></div>
+      Settings
+    </div>
+  );
+};
+
+export default Settings;
