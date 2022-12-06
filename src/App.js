b65e5c403277750c9b10d3e95c7a794a03f29e8e
aziz-codes <azizisoffline@gmail.com>
2022-12-06 11:47:04 +0500

    detect click outside setting div

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
@@ -8,7 +8,15 @@ import Settings from "./layouts/Settings";
 import { useStateContext } from "./contexts/Context";
 const App = () => {
   const { clicked, setClicked, navMenu } = useStateContext();
-
+  const ModalRef = useRef();
+  useEffect(() => {
+    window.addEventListener("click", (e) => {
+      if (ModalRef.current.contains(e.target)) {
+      } else {
+        setClicked(false);
+      }
+    });
+  });
   return (
     <div
       className={`w-full h-full flex ${
@@ -29,13 +37,24 @@ const App = () => {
           <Route path="/users" element={<Users />} />
         </Routes>
       </div>
-      <div className="fixed right-4 bottom-4 p-1 bg-slate-100 shadow-xl rounded-full">
+      <div
+        className="fixed right-4 bottom-4 p-1 bg-slate-100 shadow-xl rounded-full"
+        ref={ModalRef}
+      >
         <Cog6ToothIcon
           className="h-7 w-7 cursor-pointer hover:scale-95 transition-all ease-in-out duration-100"
           onClick={() => setClicked(!clicked)}
         />
+        {clicked && (
+          <>
+            <div className="absolute right-4 bottom-16 h-96 w-56 z-50 max-w-3xl bg-white shadow-lg border">
+              <Settings />
+
+              <div className="absolute h-4 w-4 bg-white rotate-45 -bottom-1 right-2 "></div>
+            </div>
+          </>
+        )}
       </div>
-      {clicked && <Settings />}
     </div>
   );
 };
